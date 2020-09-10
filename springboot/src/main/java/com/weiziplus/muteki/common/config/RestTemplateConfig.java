fa4a2fda5be9a9ed6c473d6b947d64b6aa052343
WeiziPlus <1602802727@qq.com>
2020-09-10 14:40:34 +0800

    1.2.8.2  restTemplate使用fastjson解析json

--- springboot/src/main/java/com/weiziplus/muteki/common/config/RestTemplateConfig.java
+++ springboot/src/main/java/com/weiziplus/muteki/common/config/RestTemplateConfig.java
@@ -1,10 +1,17 @@
 package com.weiziplus.muteki.common.config;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * @author wanglongwei
@@ -14,16 +21,35 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
-        return new RestTemplate(factory);
-    }
-
-    @Bean
     public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(15000);
         factory.setReadTimeout(5000);
         return factory;
+    }
+
+    /**
+     * 使用fastjson进行解析
+     *
+     * @param factory
+     * @return
+     */
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+        RestTemplate restTemplate = new RestTemplate(factory);
+        List<HttpMessageConverter<?>> messageConverters = restTemplate.getMessageConverters();
+        for (int i = 0; i < messageConverters.size(); i++) {
+            HttpMessageConverter<?> httpMessageConverter = messageConverters.get(i);
+            if (httpMessageConverter instanceof StringHttpMessageConverter) {
+                ((StringHttpMessageConverter) httpMessageConverter).setDefaultCharset(StandardCharsets.UTF_8);
+                continue;
+            }
+            if (httpMessageConverter instanceof MappingJackson2HttpMessageConverter) {
+                messageConverters.set(i, new FastJsonHttpMessageConverter());
+            }
+        }
+        return restTemplate;
+
     }
 
 }
